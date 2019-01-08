e8b978c7bacc4344b45d4f98434832d5249db556
isuru <iwi@masterloop.com>
2019-01-08 22:43:24 +0530

    add test project

---
+++ SchoolApiTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchoolApiTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchoolApiTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f20429a-a143-4768-961f-ecf492707c8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SchoolApiTest/StudentServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApiTest
+{
+    [TestFixture]
+    public class StudentServiceTest
+    {
+        [Test]
+        public void Test_Addition()
+        {
+            var number = 1;
+            Assert.AreEqual(1, number);
+        }
+    }
+}
